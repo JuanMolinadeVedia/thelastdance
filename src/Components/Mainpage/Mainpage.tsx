338abe642def827df2e878126c976d2c1848d44a
fbr12 <fabrizzioronco26@gmail.com>
2023-10-09 12:07:33 -0300

    feat:update MainPage=>includes NavBar and Card

--- src/Components/Mainpage/Mainpage.tsx
+++ src/Components/Mainpage/Mainpage.tsx
@@ -1,7 +1,21 @@
 import "./Mainpage.css";
+import { Card } from "../Card/Card";
+import { Navbar } from "../Navbar/Navbar";
+import { useContext } from "react";
+import { ProductContext } from "../../Context/ApiContext";
 
 function Mainpage() {
-  return <></>;
+  const product = useContext(ProductContext);
+  return (
+    <>
+      <Navbar />
+      <section>
+        {product?.map((item) => {
+          return <Card key={item.id} product={item}></Card>;
+        })}
+      </section>
+    </>
+  );
 }
 
 export { Mainpage };
