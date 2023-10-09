4bf859f3959cfff81cbf1b3c50c36defb3340538
fbr12 <fabrizzioronco26@gmail.com>
2023-10-09 12:33:27 -0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { Login } from "./Components/Login/Login";
 import { Mainpage } from "./Components/Mainpage/Mainpage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Mainpage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
