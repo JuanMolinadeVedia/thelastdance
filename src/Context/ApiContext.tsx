5c972b1a07ff99bf26fdcb9c676558ed71257ac7
GM2 Academy <gm2academy@Tecnicos-MBP-690.fibertel.com.ar>
2023-10-18 13:06:15 -0300

    chore(ApicContext): fix small issue

--- src/Context/ApiContext.tsx
+++ src/Context/ApiContext.tsx
@@ -1,11 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import { Product, ChildrenContextProps } from "../Types/Types";
 
-export const ProductContext = createContext<Product[] | undefined>(undefined);
+export const ProductContext = createContext<Product[]>([]);
 
 export function ProductContextProvider({ children }: ChildrenContextProps) {
   const PRODUCT_URL = "https://dummyjson.com/products";
-  const [info, setInfo] = useState<Product[] | undefined>(undefined);
+  const [info, setInfo] = useState<Product[]>([]);
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
