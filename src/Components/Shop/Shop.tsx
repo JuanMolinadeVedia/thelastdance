1ccbf66f1372302b86ffff10b8a7bc559983bc21
JuanelPH <jmolinadevedia@alumno.huergo.edu.ar>
2023-10-19 12:30:05 +0200

    chore(Shop): modification to make work filters

--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import { Card } from "../Card/Card";
 import { Navbar } from "../Navbar/Navbar";
 import { useContext } from "react";
 import { ProductContext } from "../../Context/ApiContext";
 import { Link } from "react-router-dom";
-import { Filters, FiltersData } from "../Filters/FIlters";
+import { Filters, FiltersData } from "../Filters/Filters";
 
 function Shop() {
   const products = useContext(ProductContext);
   const [filteredProducts, setFilteredProducts] = useState(products);
   const [prices, setPrices] = useState<number[]>([]);
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleFilterChange = (filters: FiltersData) => {
-    const { slider1Value, slider2Value } = filters;
+    const { textInput, slider1Value, slider2Value, selectedOption } = filters;
     const filtered = products.filter((product) => {
-      return product.price >= slider1Value && product.price <= slider2Value;
+      const nameMatch = product.title.toLowerCase().includes(textInput.toLowerCase());
+      const priceMatch = product.price >= slider1Value && product.price <= slider2Value;
+      const categoryMatch = selectedOption === "" || product.category === selectedOption;
+      return nameMatch && priceMatch && categoryMatch;
     });
     setFilteredProducts(filtered);
   };
@@ -22,12 +26,13 @@ function Shop() {
   useEffect(() => {
     const prices = products.map((product) => product.price);
     setPrices(prices);
+    setFilteredProducts(products);
   }, [products]);
 
   return (
     <>
       <Navbar />
-      <Filters onFilterChange={handleFilterChange} prices={prices} />
+      <Filters onFilterChange={handleFilterChange} products={products} />
       <section className="card-container">
         {filteredProducts?.map((item) => (
           <Link key={item.id} to={`/item/${item.id}`}>
