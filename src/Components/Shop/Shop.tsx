0c340054775b8384829af6b14c28124f4178b2f0
GM2 Academy <gm2academy@Tecnicos-MBP-738.fibertel.com.ar>
2023-10-20 11:23:49 -0300

    feat(Shop): Adaptation to work onto Sidebar

--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -4,25 +4,34 @@ import { Navbar } from "../Navbar/Navbar";
 import { useContext } from "react";
 import { ProductContext } from "../../Context/ApiContext";
 import { Link } from "react-router-dom";
-import { Filters, FiltersData } from "../Filters/Filters";
+import { FiltersData } from "../Filters/Filters";
+import { Sidebar } from "../Sidebar/Sidebar";
 
 function Shop() {
   const products = useContext(ProductContext);
   const [filteredProducts, setFilteredProducts] = useState(products);
   const [prices, setPrices] = useState<number[]>([]);
   const [selectedOption, setSelectedOption] = useState("");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const handleFilterChange = (filters: FiltersData) => {
     const { textInput, slider1Value, slider2Value, selectedOption } = filters;
     const filtered = products.filter((product) => {
-      const nameMatch = product.title.toLowerCase().includes(textInput.toLowerCase());
-      const priceMatch = product.price >= slider1Value && product.price <= slider2Value;
-      const categoryMatch = selectedOption === "" || product.category === selectedOption;
+      const nameMatch = product.title
+        .toLowerCase()
+        .includes(textInput.toLowerCase());
+      const priceMatch =
+        product.price >= slider1Value && product.price <= slider2Value;
+      const categoryMatch =
+        selectedOption === "" || product.category === selectedOption;
       return nameMatch && priceMatch && categoryMatch;
     });
     setFilteredProducts(filtered);
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   useEffect(() => {
     const prices = products.map((product) => product.price);
@@ -30,18 +39,24 @@ function Shop() {
     setFilteredProducts(products);
   }, [products]);
 
-
   return (
     <>
       <Navbar />
-      <Filters onFilterChange={handleFilterChange} products={products} />
-      <section className="card-container">
-        {filteredProducts?.map((item) => (
-          <Link key={item.id} to={`/item/${item.id}`}>
-            <Card product={item} />
-          </Link>
-        ))}
-      </section>
+      <div className="container">
+        <button className="sidebar-toggle" onClick={toggleSidebar}>
+          Display Filters
+        </button>
+        {isSidebarOpen && (
+          <Sidebar onFilterChange={handleFilterChange} products={products} />
+        )}
+        <section className="card-container">
+          {filteredProducts?.map((item) => (
+            <Link key={item.id} to={`/item/${item.id}`}>
+              <Card product={item} />
+            </Link>
+          ))}
+        </section>
+      </div>
     </>
   );
 }
