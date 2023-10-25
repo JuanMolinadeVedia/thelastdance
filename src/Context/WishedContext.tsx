262c6d284e9f75b383e522a40f148acdb8ee65f0
fbr12 <fabrizzioronco26@gmail.com>
2023-10-25 12:34:52 -0300

    fix(Wished,Carted-Context):do not add if already exists

--- src/Context/WishedContext.tsx
+++ src/Context/WishedContext.tsx
@@ -13,38 +13,39 @@ const DEFAULT_VALUE = {
 };
 
 const getWished = (id: number) => {
-  const wishedString = sessionStorage.getItem(`wished_${id}`);
-  const wishedData: Product[] = JSON.parse(wishedString);
-  return wishedData
+  const wishedString = localStorage.getItem(`wished_${id}`);
+  if (wishedString) {
+    const wishedData: Product[] = JSON.parse(wishedString);
+    return wishedData;
+  }
 };
 
-
 const saveWished = (id: number, wishedData: Product[]) => {
-  sessionStorage.setItem(`wished_${id}`, JSON.stringify(wishedData));
+  localStorage.setItem(`wished_${id}`, JSON.stringify(wishedData));
 };
 
 export const WishedContext = createContext<WishedContextValue>(DEFAULT_VALUE);
 
 export function WishedContextProvider({ children }: ChildrenContextProps) {
-
-  const { loggedUserInfo } = useContext(IsLoggedContext)
-  const wishedData = getWished(loggedUserInfo.id)
+  const { loggedUserInfo } = useContext(IsLoggedContext);
+  const wishedData = getWished(loggedUserInfo.id);
   const [wished, setWished] = useState<Product[]>(wishedData ? wishedData : []);
 
   const handleClickWish = useCallback(
     (p: Product) => {
-      console.log(wished);
-      setWished([...wished, p]);
-      saveWished(loggedUserInfo.id, [...wished, p])
-      console.log(getWished(loggedUserInfo.id))
+      const isIn = wished.some((product) => product.id === p.id);
+      if (!isIn) setWished([...wished, p]);
+      saveWished(loggedUserInfo.id, [...wished, p]);
     },
-    [wished]
+    [wished, loggedUserInfo.id]
   );
   return (
-    <WishedContext.Provider value={{
-      wishedList: wished,
-      clickFunctionWish: handleClickWish,
-    }}>
+    <WishedContext.Provider
+      value={{
+        wishedList: wished,
+        clickFunctionWish: handleClickWish,
+      }}
+    >
       {children}
     </WishedContext.Provider>
   );
